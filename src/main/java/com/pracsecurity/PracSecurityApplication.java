d3edc2e0ab76f3eae01f5259fb35be0740c133bf
torahoo <legokbs@gmail.com>
2023-11-09 17:28:02 +0900

    #1 setting 프로젝트 설정 후 깃에 업로드

---
+++ src/main/java/com/pracsecurity/PracSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.pracsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracSecurityApplication.class, args);
+    }
+
+}
