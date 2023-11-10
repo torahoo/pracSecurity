69c510b1ebeab668500542c5f8c22399cd7d2ea8
torahoo <legokbs@gmail.com>
2023-11-10 11:05:08 +0900

    #3 makeController 시큐리티를 적용하기전 프로젝트 엔티티 및 controller 세팅 error : index 테스트 과정에서 yml 세팅이 잘못되어 전 프로젝트에서 설정을 끌어옴. 하지만 시큐리티 설정 때문에 index가 보이지 않는 문제가 발생하여 주석 처리함. ==> 해결 (index 보임)

--- src/main/java/com/pracsecurity/controller/BaseController.java
+++ src/main/java/com/pracsecurity/controller/BaseController.java
@@ -1,6 +1,15 @@
 package com.pracsecurity.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class BaseController {
 
+    @RequestMapping("/")
+    public String index () {
+
+        return "index";
+    }
 
 }
